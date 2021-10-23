248e78bcb1e1956ae0f90137af5988bd8794e51b
Bernhard Spindler <bernhard.spindler.75@gmail.com>
2021-10-23 15:26:28 +0200

    beautify

--- martin.sh
+++ martin.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-  sed -e 's/;/\n/g' $1 \
-| sed -e '/^$/d' \
+  sed 's/;/\n/g;/^$/d' $1 \
+| sed '/^$/d' \
 | tr [:lower:] [:upper:] \
-| awk '{ split($2,e,"+"); split(e[1],v,"."); printf "%s %04d.%04d.%04d%s %s\n", $1,v[1],v[2],v[3],(e[2]==""?"":"+"e[2]),$2 }' \
+| awk '{ split($2,e,"+");   \
+         split(e[1],v,"."); \
+         printf "%s %04d.%04d.%04d%s %s\n", $1,v[1],v[2],v[3],(e[2]==""?"":"+"e[2]),$2 }' \
 | sort --reverse \
 | sort  -k1,1 -t' ' --unique \
 | cut -d" " -f1,3
