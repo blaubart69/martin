55bede949e1c4d513466164bef38adde74b5f893
Bernhard Spindler <bernhard.spindler.75@gmail.com>
2021-10-23 15:20:51 +0200

    with 2nd awk call

--- martin.sh
+++ martin.sh
@@ -2,7 +2,7 @@
   sed -e 's/;/\n/g' $1 \
 | sed -e '/^$/d' \
 | tr [:lower:] [:upper:] \
-| awk '{ split($2,e,"+"); split(e[1],v,"."); printf "%s %04d.%04d.%04d%s\n", $1,v[1],v[2],v[3],(e[2]==""?"":"+"e[2])  }' \
+| awk '{ split($2,e,"+"); split(e[1],v,"."); printf "%s %04d.%04d.%04d%s %s\n", $1,v[1],v[2],v[3],(e[2]==""?"":"+"e[2]),$2 }' \
 | sort --reverse \
 | sort  -k1,1 -t' ' --unique \
-| awk '{ split($2,e,"+"); split(e[1],v,"."); printf "%s %d.%d.%d%s\n", $1,v[1],v[2],v[3],(e[2]==""?"":"+"e[2])  }'
+| cut -d" " -f1,3
