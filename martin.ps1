c5f7975e898c2c20903cf0ea8746bba174ce2f89
Bernhard Spindler <bernhard.spindler.75@gmail.com>
2021-10-24 07:47:29 +0200

    powershell version

---
+++ martin.ps1
@@ -0,0 +1,15 @@
+gc "in.csv" |
+% { ($_ -split ';') -ne "" } |
+% { $name,$version =  $_ -split '\s+'
+    $versionWithoutPlusSuffix = ($version | Select-String -Pattern '[\d\.]+').Matches.Value -split '\.'
+
+    [PSCustomObject]@{
+        Name            = $name.ToUpper()
+        Version         = $version 
+        VersionSortable = $versionWithoutPlusSuffix | % { $_.PadLeft(5,'0') } | Join-String
+    }
+} |
+Sort-Object  -Property Name,VersionSortable -Descending -Unique |
+Group-Object -Property Name | 
+% { $_.Group[0] } |
+Select-Object -Property Name,Version
